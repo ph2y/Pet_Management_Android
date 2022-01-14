e289d5127ea339e5e24a4cb69cb6dd8c9a90a825
HanJuK <qwerty03081@gmail.com>
2022-01-14 15:45:01 +0900

    CreateUpdatePost: implemented file size limit check

--- app/src/main/java/com/sju18001/petmanagement/restapi/global/FileType.kt
+++ app/src/main/java/com/sju18001/petmanagement/restapi/global/FileType.kt
@@ -5,4 +5,8 @@ object FileType {
     const val IMAGE_FILE: String = "IMAGE_FILE"
     const val VIDEO_FILE: String = "VIDEO_FILE"
     const val AUDIO_FILE: String = "AUDIO_FILE"
+
+    const val FILE_SIZE_LIMIT_PHOTO: Long = (20 * 1000000).toLong()
+    const val FILE_SIZE_LIMIT_VIDEO: Long = (100 * 1000000).toLong()
+    const val FILE_SIZE_LIMIT_GENERAL: Long = (100 * 1000000).toLong()
 }
