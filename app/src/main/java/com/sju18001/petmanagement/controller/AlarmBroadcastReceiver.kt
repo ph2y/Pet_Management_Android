46ae8e6573f9fa5e725ff5e73bf4eb27b3a9528b
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-01-18 15:08:40 +0900

    Implement re-adding alarm logic called after reboot

--- app/src/main/java/com/sju18001/petmanagement/controller/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/sju18001/petmanagement/controller/AlarmBroadcastReceiver.kt
@@ -6,11 +6,18 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.sju18001.petmanagement.R
 import com.sju18001.petmanagement.SplashActivity
+import com.sju18001.petmanagement.restapi.RetrofitBuilder
+import com.sju18001.petmanagement.restapi.ServerUtil
+import com.sju18001.petmanagement.restapi.dao.Pet
+import com.sju18001.petmanagement.restapi.dto.FetchPetReqDto
 import com.sju18001.petmanagement.ui.login.LoginActivity
+import com.sju18001.petmanagement.ui.myPet.petScheduleManager.PetScheduleNotification
+import java.util.HashMap
 
 class AlarmBroadcastReceiver : BroadcastReceiver() {
     companion object{
@@ -50,6 +57,44 @@ class AlarmBroadcastReceiver : BroadcastReceiver() {
                 // 알림 띄우기
                 with(NotificationManagerCompat.from(context)){
                     notify(NOTIFICATION_ID, builder.build())
+                }
+            }
+            "android.intent.action.BOOT_COMPLETED" -> {
+                if(context != null){
+                    /*
+                     * 재부팅 시 알람이 모두 삭제되었으므로, 다시 등록합니다.
+                     * 1. fetch pet -> petNameForId 생성
+                     * 2. fetch pet schedule -> enabled 상태인 스케줄들 모두 등록
+                     */
+
+                    // Fetch pet
+                    val call = RetrofitBuilder.getServerApiWithToken(SessionManager.fetchUserToken(context)!!)
+                        .fetchPetReq(FetchPetReqDto( null , null))
+                    ServerUtil.enqueueApiCall(call, {false}, context, { response ->
+                        // Set petNameForId
+                        val petNameForId = HashMap<Long, String>()
+                        response.body()?.petList?.map {
+                            petNameForId[it.id] = it.name
+                        }
+
+                        // Fetch pet schedule and Add the schedules to alarm manager
+                        val call = RetrofitBuilder.getServerApiWithToken(SessionManager.fetchUserToken(context)!!)
+                            .fetchPetScheduleReq(ServerUtil.getEmptyBody())
+                        ServerUtil.enqueueApiCall(call, {false}, context, { response ->
+                            // ON인 것들에 대해 알림 설정
+                            response.body()?.petScheduleList?.map{
+                                if(it.enabled){
+                                    PetScheduleNotification.setAlarmManagerRepeating(
+                                        context.applicationContext,
+                                        it.id,
+                                        it.time,
+                                        Util.getPetNamesFromPetIdList(petNameForId, it.petIdList),
+                                        it.memo
+                                    )
+                                }
+                            }
+                        }, {}, {})
+                    },{},{})
                 }
             }
         }
