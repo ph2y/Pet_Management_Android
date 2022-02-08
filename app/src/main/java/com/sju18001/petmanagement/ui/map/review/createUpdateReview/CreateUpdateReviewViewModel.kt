b6548186baf7dd586f0213ead4701d195c0660cd
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-02-08 21:05:52 +0900

    Implement star image data binding

--- app/src/main/java/com/sju18001/petmanagement/ui/map/review/createUpdateReview/CreateUpdateReviewViewModel.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/map/review/createUpdateReview/CreateUpdateReviewViewModel.kt
@@ -1,7 +1,9 @@
 package com.sju18001.petmanagement.ui.map.review.createUpdateReview
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 
 class CreateUpdateReviewViewModel: ViewModel() {
     var fragmentType = 0
+    var rating: ObservableField<Int> = ObservableField(0)
 }
