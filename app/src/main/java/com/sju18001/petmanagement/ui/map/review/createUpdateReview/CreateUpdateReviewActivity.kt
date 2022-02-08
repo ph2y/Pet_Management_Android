51d0a0dac15d951d88ac4a5f8c02a392dd73f865
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-02-08 18:01:41 +0900

    Implement back button

--- app/src/main/java/com/sju18001/petmanagement/ui/map/review/createUpdateReview/CreateUpdateReviewActivity.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/map/review/createUpdateReview/CreateUpdateReviewActivity.kt
@@ -2,6 +2,8 @@ package com.sju18001.petmanagement.ui.map.review.createUpdateReview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.sju18001.petmanagement.R
@@ -19,17 +21,41 @@ class CreateUpdateReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_update_review)
+        setBinding()
+
+        supportActionBar?.hide()
+    }
+
+    private fun setBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_update_review)
 
         binding.lifecycleOwner = this
+        binding.activity = this@CreateUpdateReviewActivity
         binding.viewModel = model
-
-        supportActionBar?.hide()
     }
 
     override fun onStart() {
         super.onStart()
 
+        initializeViewModelWithExtra()
+    }
+
+    private fun initializeViewModelWithExtra() {
         model.fragmentType = intent.getIntExtra("fragmentType", 0)
+        // TODO: review data for update
+    }
+
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.enter_from_left, R.anim.exit_to_right)
+    }
+
+
+    /**
+     * xml의 onClick에서 처리하는 영역입니다. 따라서 따로 리스너를 달지 않아도 됩니다.
+     */
+    fun onBackButtonClicked() {
+        finish()
     }
 }
