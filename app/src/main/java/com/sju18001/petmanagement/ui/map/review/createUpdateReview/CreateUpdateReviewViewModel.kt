8e404abf4437c61cc21f176cbe84dbcfc5f5f508
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-02-08 22:01:00 +0900

    Implement fetching review for UPDATE_REVIEW fragment

--- app/src/main/java/com/sju18001/petmanagement/ui/map/review/createUpdateReview/CreateUpdateReviewViewModel.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/map/review/createUpdateReview/CreateUpdateReviewViewModel.kt
@@ -5,7 +5,12 @@ import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 
 class CreateUpdateReviewViewModel: ViewModel() {
-    var fragmentType = 0
+    var fragmentType: Int = 0
+    var reviewId: Long = -1 // update review일 때 필요합니다.
+
+    var isReviewFetched: Boolean = false
+
+    // UPDATE_REVIEW일 때, FetchReview 이후에 아래 값들을 불러옵니다.
     var rating: ObservableField<Int> = ObservableField(0)
     var contents: ObservableField<String> = ObservableField("")
 }
