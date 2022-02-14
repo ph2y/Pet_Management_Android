6089a8d1a5466d261df6ba5a7670d15875a6d7ea
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-02-14 21:47:49 +0900

    Refactor MapFragment

--- app/src/main/java/com/sju18001/petmanagement/ui/map/MapViewModel.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/map/MapViewModel.kt
@@ -1,9 +1,11 @@
 package com.sju18001.petmanagement.ui.map
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MapViewModel : ViewModel() {
-
+    var searchText: ObservableField<String> = ObservableField("")
+    var placeIdInLocationInformation: ObservableField<Int> = ObservableField(1)
 }
