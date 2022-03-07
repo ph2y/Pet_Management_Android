622e93c6f67f49f4a4ff94c58b4a8807da99f143
HanJuK <qwerty03081@gmail.com>
2022-03-08 00:34:00 +0900

    add removeProgressBar logic for certain fragments

--- app/src/main/java/com/sju18001/petmanagement/ui/myPet/petScheduleManager/PetScheduleManagerFragment.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/myPet/petScheduleManager/PetScheduleManagerFragment.kt
@@ -15,6 +15,7 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.sju18001.petmanagement.R
 import com.sju18001.petmanagement.controller.CustomProgressBar
@@ -61,6 +62,11 @@ class PetScheduleManagerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.adView.loadAd(AdRequest.Builder().build())
+        binding.adView.adListener = object: AdListener() {
+            override fun onAdLoaded() {
+                CustomProgressBar.removeProgressBar(binding.fragmentPetScheduleManagerParentLayout)
+            }
+        }
     }
 
     override fun onResume() {
