ef644a576ac03e57e5b0d783c2e8ae7d634b66bf
HanJu Kim <48195650+HanJuK@users.noreply.github.com>
2022-03-08 22:56:46 +0900

    removed removeProgressBar logic

--- app/src/main/java/com/sju18001/petmanagement/ui/community/comment/CommentFragment.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/community/comment/CommentFragment.kt
@@ -14,7 +14,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.sju18001.petmanagement.R
 import com.sju18001.petmanagement.controller.CustomProgressBar
@@ -98,12 +97,6 @@ class CommentFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.adView.loadAd(AdRequest.Builder().build())
-        binding.adView.adListener = object: AdListener() {
-            override fun onAdLoaded() {
-                CustomProgressBar.removeProgressBar(binding.fragmentCommentParentLayout)
-                binding.layoutSwipeRefresh.isRefreshing = false
-            }
-        }
     }
 
     override fun onDestroyView() {
