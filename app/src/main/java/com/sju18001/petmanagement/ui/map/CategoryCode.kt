5919ace07b1fd5f9114507dbdbfc0ac95b60cd68
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-03-08 19:12:50 +0900

    Add CategoryCode enum class, Rename supply to shop and grooming to salon for shortcut

---
+++ app/src/main/java/com/sju18001/petmanagement/ui/map/CategoryCode.kt
@@ -0,0 +1,11 @@
+package com.sju18001.petmanagement.ui.map
+
+enum class CategoryCode(categoryCode: String) {
+    SHOP("shop"), // 매장
+    HOSPITAL("hospital"), // 병원
+    HOTEL("hotel"), // 호텔
+    CAFE("cafe"), // 카페
+    SALON("salon"), // 미용실
+    PARK("park"), // 공원
+    SHELTER("shelter"); // 보호소
+}
