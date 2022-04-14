dd0669984d6f347bf22ba8f94117437bd80fcb28
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-04-14 21:33:10 +0900

    Remove unused file

--- app/src/main/java/com/sju18001/petmanagement/ui/myPet/petScheduleManager/PetScheduleManagerViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.sju18001.petmanagement.ui.myPet.petScheduleManager
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class PetScheduleManagerViewModel: ViewModel() {
-    var isApiLoading = MutableLiveData(false)
-}
