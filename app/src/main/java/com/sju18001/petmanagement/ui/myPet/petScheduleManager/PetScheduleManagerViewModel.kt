9d53ce790cffe08541251a22ff720b0119f3e626
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-04-14 21:32:46 +0900

    Refactor PetScheduleManagerFragment using databinding

---
+++ app/src/main/java/com/sju18001/petmanagement/ui/myPet/petScheduleManager/PetScheduleManagerViewModel.kt
@@ -0,0 +1,8 @@
+package com.sju18001.petmanagement.ui.myPet.petScheduleManager
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PetScheduleManagerViewModel: ViewModel() {
+    var isApiLoading = MutableLiveData(false)
+}
