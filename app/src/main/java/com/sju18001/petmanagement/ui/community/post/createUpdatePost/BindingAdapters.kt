39747120e7dcecd528ce9bed4299f8993613cc04
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-04-24 22:26:16 +0900

    Refactor CreateUpdatePostPetSelectorAdapter using databinding

--- app/src/main/java/com/sju18001/petmanagement/ui/community/post/createUpdatePost/BindingAdapters.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/community/post/createUpdatePost/BindingAdapters.kt
@@ -1,7 +1,11 @@
 package com.sju18001.petmanagement.ui.community.post.createUpdatePost
 
+import android.content.Context
 import android.view.View
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.sju18001.petmanagement.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 object BindingAdapters {
     @JvmStatic
@@ -13,5 +17,19 @@ object BindingAdapters {
         isApiLoading: Boolean
     ) {
         view.isEnabled = contents.trim().isNotEmpty() && selectedPetId != null && !isApiLoading
+    }
+
+    @JvmStatic
+    @BindingAdapter("petSelectorItem", "context")
+    fun bindPetPhoto(
+        petPhotoCircleImageView: CircleImageView,
+        petSelectorItem: CreateUpdatePostPetSelectorItem,
+        context: Context
+    ) {
+        if(petSelectorItem.petPhotoUrl != null){
+            Glide.with(context).load(petSelectorItem.petPhoto).into(petPhotoCircleImageView)
+        }else{
+            petPhotoCircleImageView.setImageResource(R.drawable.ic_baseline_pets_60_with_padding)
+        }
     }
 }
