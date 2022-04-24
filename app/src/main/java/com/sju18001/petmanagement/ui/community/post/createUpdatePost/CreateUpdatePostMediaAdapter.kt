bf2ea81f6b6768766fedfb3b6945a07c5e7dbadf
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-04-24 22:59:06 +0900

    Refactor CreateUpdatePostMediaAdapter using databinding

--- app/src/main/java/com/sju18001/petmanagement/ui/community/post/createUpdatePost/CreateUpdatePostMediaAdapter.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/community/post/createUpdatePost/CreateUpdatePostMediaAdapter.kt
@@ -6,69 +6,70 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.sju18001.petmanagement.R
 import com.sju18001.petmanagement.databinding.ActivityCreateupdatepostBinding
+import com.sju18001.petmanagement.databinding.ItemCreateupdatepostmediaBinding
 import java.io.File
 
 class CreateUpdatePostMediaAdapter(
     private val context: Context,
     private val createUpdatePostViewModel: CreateUpdatePostViewModel
     )
-    : RecyclerView.Adapter<CreateUpdatePostMediaAdapter.HistoryListViewHolder>() {
+    : RecyclerView.Adapter<CreateUpdatePostMediaAdapter.ViewHolder>() {
     private var dataSet = mutableListOf<CreateUpdatePostMedia>()
 
-    class HistoryListViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val thumbnail: ImageView = view.findViewById(R.id.imageview_thumbnail)
-        val deleteButton: ImageView = view.findViewById(R.id.imageview_deletebutton)
-    }
+    class ViewHolder(
+        private val adapter: CreateUpdatePostMediaAdapter,
+        private val binding: ItemCreateupdatepostmediaBinding,
+        private val context: Context,
+        private val createUpdatePostViewModel: CreateUpdatePostViewModel
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(createUpdatePostMedia: CreateUpdatePostMedia) {
+            binding.adapter = adapter
+            binding.holder = this
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryListViewHolder {
-        val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_createupdatepostmedia, parent, false)
+            binding.executePendingBindings()
 
-        val holder = HistoryListViewHolder(view)
-        setListenerOnView(holder)
-
-        return holder
-    }
-
-    override fun onBindViewHolder(holder: HistoryListViewHolder, position: Int) {
-        if (dataSet[position].isVideo){
-            Glide.with(context)
-                .load(createUpdatePostViewModel.videoPathList.value!![dataSet[position].indexInList])
-                .placeholder(R.drawable.ic_baseline_video_library_36)
-                .into(holder.thumbnail)
-        }else{
-            Glide.with(context)
-                .load(File(createUpdatePostViewModel.photoPathList.value!![dataSet[position].indexInList]))
-                .into(holder.thumbnail)
+            if (createUpdatePostMedia.isVideo){
+                Glide.with(context)
+                    .load(createUpdatePostViewModel.videoPathList.value!![createUpdatePostMedia.indexInList])
+                    .placeholder(R.drawable.ic_baseline_video_library_36)
+                    .into(binding.imageviewThumbnail)
+            }else{
+                Glide.with(context)
+                    .load(File(createUpdatePostViewModel.photoPathList.value!![createUpdatePostMedia.indexInList]))
+                    .into(binding.imageviewThumbnail)
+            }
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = DataBindingUtil.inflate<ItemCreateupdatepostmediaBinding>(LayoutInflater.from(parent.context),
+            R.layout.item_createupdatepostmedia, parent, false)
+        return ViewHolder(this, binding, context, createUpdatePostViewModel)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(dataSet[position])
     }
 
     override fun getItemCount() = dataSet.size
 
-    private fun setListenerOnView(holder: HistoryListViewHolder) {
-        holder.deleteButton.setOnClickListener {
-            val position = holder.absoluteAdapterPosition
-            if (dataSet[position].isVideo) deleteVideo(position)
-            else deletePhoto(position)
-        }
+    fun getDataSet() = dataSet
+
+    fun addItem(item: CreateUpdatePostMedia) {
+        dataSet.add(item)
     }
 
-    private fun deletePhoto(position: Int) {
-        File(dataSet[position].path).delete()
 
-        createUpdatePostViewModel.removePhotoPath(dataSet[position].path)
-        dataSet.removeAt(position)
-        notifyItemRemoved(position)
-
-        // Photo들의 index를 다시 구성합니다.
-        var newIndex = 0
-        for (i in 0 until itemCount) {
-            if (!dataSet[i].isVideo) dataSet[i].indexInList = newIndex++
-        }
+    /** Databinding functions */
+    fun onClickDeleteButton(holder: ViewHolder) {
+        val position = holder.absoluteAdapterPosition
+        if (dataSet[position].isVideo) deleteVideo(position)
+        else deletePhoto(position)
     }
 
     private fun deleteVideo(position: Int) {
@@ -85,9 +86,17 @@ class CreateUpdatePostMediaAdapter(
         }
     }
 
-    fun getDataSet() = dataSet
+    private fun deletePhoto(position: Int) {
+        File(dataSet[position].path).delete()
 
-    fun addItem(item: CreateUpdatePostMedia) {
-        dataSet.add(item)
+        createUpdatePostViewModel.removePhotoPath(dataSet[position].path)
+        dataSet.removeAt(position)
+        notifyItemRemoved(position)
+
+        // Photo들의 index를 다시 구성합니다.
+        var newIndex = 0
+        for (i in 0 until itemCount) {
+            if (!dataSet[i].isVideo) dataSet[i].indexInList = newIndex++
+        }
     }
 }
