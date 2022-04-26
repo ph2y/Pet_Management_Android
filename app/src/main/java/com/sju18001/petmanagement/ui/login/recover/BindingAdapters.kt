2e201472198c39e23c630f9e0f0cef7c87bb1552
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-04-27 00:20:25 +0900

    Refactor RecoverPasswordFragment

--- app/src/main/java/com/sju18001/petmanagement/ui/login/recover/BindingAdapters.kt
+++
@@ -1,9 +0,0 @@
-package com.sju18001.petmanagement.ui.login.recover
-
-import android.view.View
-import androidx.databinding.BindingAdapter
-import com.sju18001.petmanagement.controller.PatternRegex
-
-object BindingAdapters {
-
-}
