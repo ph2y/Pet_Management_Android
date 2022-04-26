17a804faf97ae45c901d8365acc4524d430338a1
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2022-04-26 23:33:42 +0900

    Refactor RecoverUsernameFragment

---
+++ app/src/main/java/com/sju18001/petmanagement/ui/login/recover/BindingAdapters.kt
@@ -0,0 +1,9 @@
+package com.sju18001.petmanagement.ui.login.recover
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import com.sju18001.petmanagement.controller.PatternRegex
+
+object BindingAdapters {
+
+}
