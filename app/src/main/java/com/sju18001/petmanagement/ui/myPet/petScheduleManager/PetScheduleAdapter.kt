60c5fe17c3ef03b67fdbcc509c8be98a506c69b3
강한빛 <fchopinof99@gmail.com>
2022-05-01 14:29:42 +0900

    Divide BindingAdpaters

--- app/src/main/java/com/sju18001/petmanagement/ui/myPet/petScheduleManager/PetScheduleAdapter.kt
+++ app/src/main/java/com/sju18001/petmanagement/ui/myPet/petScheduleManager/PetScheduleAdapter.kt
@@ -68,37 +68,6 @@ class PetScheduleAdapter(
 
 
     /** databinding functions */
-    companion object {
-        @JvmStatic
-        @BindingAdapter("setNoonTextView")
-        fun setNoonTextView(textView: TextView, time: String) {
-            val localTime = LocalTime.parse(time)
-            textView.text = if(localTime.hour <= 12) "오전" else "오후"
-        }
-
-        @JvmStatic
-        @BindingAdapter("setTimeTextView")
-        fun setTimeTextView(textView: TextView, time: String) {
-            val localTime = LocalTime.parse(time)
-            textView.text = localTime.hour.toString().padStart(2, '0') +
-                    ":" + localTime.minute.toString().padStart(2, '0')
-        }
-
-        @JvmStatic
-        @BindingAdapter("petNameForId", "petIdList")
-        fun setPetListView(textView: TextView, petNameForId: HashMap<Long, String>, petIdList: String) {
-            textView.text = Util.getPetNamesFromPetIdList(petNameForId, petIdList)
-        }
-
-        @JvmStatic
-        @BindingAdapter("layout_marginLeft")
-        fun setMarginLeft(view: View, dp: Int) {
-            val layoutParams = view.layoutParams as ViewGroup.MarginLayoutParams
-            layoutParams.leftMargin = dp
-            view.layoutParams = layoutParams
-        }
-    }
-
     fun onClickPetScheduleItem(holder: ViewHolder) {
         val position = holder.absoluteAdapterPosition
         petScheduleAdapterInterface.startCreateUpdatePetScheduleFragmentForUpdate(dataSet[position])
